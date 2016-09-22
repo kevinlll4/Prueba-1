c94441296965d3694ea2ce5c6adfd7078a87fcff
kevin <kevin>
2016-09-22 07:30:55 -0500

    Agregando estructura

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express=require("express");
 var app=express();
+var config=require("./config/config.cfg");
 
+app.get("/holax",function(req,res){
+	res.send("Probando holax");
+})
 app.get("/",function(req,res){
 res.send("Prueba 1");
 });
